19f2a72a9a0d658dcf771f1742bcc4aa73936e3a
sandeepmukku <sandeep12akp@gmail.com>
2026-01-21 23:53:39 +0530

    Register task routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.route.js";
+import taskRoutes from "./task.route.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/tasks", taskRoutes);
 
 export default router;
