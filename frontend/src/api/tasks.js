4d4fa435e5ba2016dd4664b822bc985d903a95ea
sandeepmukku <sandeep12akp@gmail.com>
2026-01-22 03:00:31 +0530

    Setup Tasks API Using Axios Interceptors

---
+++ frontend/src/api/tasks.js
@@ -0,0 +1,15 @@
+import axiosInstance from "./axiosInstance";
+
+const TASKS_BASE_URL = "http://localhost:8082";
+
+export const getTasks = (userId) =>
+  axiosInstance.get(`${TASKS_BASE_URL}/tasks?user_id=${userId}`);
+
+export const createTask = (task) =>
+  axiosInstance.post(`${TASKS_BASE_URL}/tasks`, task);
+
+export const updateTask = (taskId, updates) =>
+  axiosInstance.put(`${TASKS_BASE_URL}/tasks/${taskId}`, updates);
+
+export const deleteTask = (taskId) =>
+  axiosInstance.delete(`${TASKS_BASE_URL}/tasks/${taskId}`);
